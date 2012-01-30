9e3ba665eeae645a3c259792ff5a4cd7c4808144
PsychoMario <pink.banana.fish@gmail.com>
2012-01-30 18:20:48 +0000

    Made tftpd.py actually work for larger files, wasnt sending eof properly.

--- tftpd.py
+++ tftpd.py
@@ -11,10 +11,13 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host, port))
 
 def group(s, n): return [s[i:i+n] for i in xrange(0, len(s), n)]
-
+last=()
+import time
 while 1:
     try:
         message, address = s.recvfrom(8192)
+        print repr(message)
+        if last == address: continue
         if message.startswith('\x00\x01'): #WRQ
            message=message.split('\x00')
            filename=message[1][1:]
@@ -23,18 +26,20 @@ while 1:
               s.sendto('\x00\x05\x00\x01no such file exists',address)
               continue
            fsize=os.path.getsize('.'+filename)
-           s.sendto('\x00\x06blksize\x001024\x00tsize\x00%s\x00' % fsize,address) ##512
+           s.sendto('\x00\x06blksize\x00512\x00tsize\x00%s\x00' % fsize,address)
         elif message.startswith('\x00\x04'): #OptACK
+           last=address
            f=open('.'+filename,'r')
            data=f.read()
-           dataset=group(data,1024) ##512
+           dataset=group(data,512)
            if len(dataset) > 65534: print "Won't work, too large... >64MB"
            for index,chunk in enumerate(dataset):
-              index+=1
               try:
-                 s.sendto('\x00\x03'+binascii.unhexlify(hex(index)[2:].rjust(4,'0'))+chunk,address)
+                 s.sendto('\x00\x03'+binascii.unhexlify(hex(index+1)[2:].rjust(4,'0'))+chunk,address)
               except TypeError:
                  break
               s.recvfrom(128)
+           s.sendto('\x00\x03'+binascii.unhexlify(hex(index+2)[2:].rjust(4,'0')),address)
+           f.close()
     except KeyboardInterrupt:
         exit()
