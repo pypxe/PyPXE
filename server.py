6ce7690fea03eae7d4afa557fd10c746261dea66
PsychoMario <pink.banana.fish@gmail.com>
2014-07-08 19:41:11 +0100

    implemented some of mmattioli's settings

--- server.py
+++ server.py
@@ -6,31 +6,41 @@ from tftpd import TFTPD
 from dhcpd import DHCPD
 if __name__ == '__main__':
     
-    os.chdir("netboot")
-    USEIPXE = True #boot into ipxe first, then filename
-    USEHTTP = True #filename is on fileserver as http
-    USEDHCP = True #using DHCP server written above
-    PROXYDHCP = True
+    #PyPXE Global Settings
+    os.chdir("netboot") #local boot file directory
+    USE_IPXE = True #boot into ipxe first, then filename
+    USE_HTTP = True #filename is on fileserver as http
+    USE_DHCP = True #enable/disable built-in DHCP server
+    
+    #DHCP Server Settings
+    DHCP_PROXYDHCP = True
+    DHCP_SERVER_IP = '192.168.2.2'
+    DHCP_FILESERVER_IP = '192.168.2.2'
+    DHCP_OFFER_BEGIN = '192.168.2.100'
+    DHCP_OFFER_END = '192.168.2.150'
+    DHCP_SUBNET = '255.255.255.0'
+    DHCP_ROUTER = '192.168.2.1'
+    DHCP_DNS = '8.8.8.8'
 
-    if not USEIPXE:
-        filename = "/pxelinux.0"
-    elif not USEHTTP:
-        filename = "/boot.ipxe"
+    if not USE_IPXE:
+        DHCP_FILENAME = "/pxelinux.0"
+    elif not USE_HTTP:
+        DHCP_FILENAME = "/boot.ipxe"
     else:
-        filename = "/boot.http.ipxe"
+        DHCP_FILENAME = "/boot.http.ipxe"
 
     tftpd = TFTPD()
     tftpthread = threading.Thread(target=tftpd.listen)
     tftpthread.daemon = True
     tftpthread.start()
 
-    if USEDHCP:
-        dhcpd = DHCPD('192.168.2.2', '192.168.2.100', '192.168.2.150', '255.255.255.0', '192.168.2.1', '8.8.8.8', filename, '192.168.2.2', USEIPXE, USEHTTP, PROXYDHCP)
+    if USE_DHCP:
+        dhcpd = DHCPD(DHCP_FILESERVER_IP, DHCP_OFFER_BEGIN, DHCP_OFFER_END, DHCP_SUBNET, DHCP_ROUTER, DHCP_DNS, DHCP_FILENAME, DHCP_SERVER_IP, USE_IPXE, USE_HTTP, DHCP_PROXYDHCP)
         dhcpthread = threading.Thread(target=dhcpd.listen)
         dhcpthread.daemon = True
         dhcpthread.start()
 
-    if USEHTTP:
+    if USE_HTTP:
         httpd = HTTPD()
         httpdthread = threading.Thread(target=httpd.listen)
         httpdthread.daemon = True
