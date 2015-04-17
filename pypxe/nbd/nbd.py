35f3e1588e3a68ba6c34c57207908414daef6f16
PsychoMario <pink.banana.fish@gmail.com>
2015-04-17 18:42:17 +0100

    rewrote handshake, readonly hangs

--- pypxe/nbd/nbd.py
+++ pypxe/nbd/nbd.py
@@ -39,72 +39,70 @@ class NBD:
         self.logger.debug('  NBD Server Port: {}'.format(self.port))
         self.logger.debug('  NBD Block Device: {}'.format(self.bd))
 
+    def sendreply(self, conn, addr, code, data):
+        reply = struct.pack("!Q", 0x3e889045565a9)
+        reply += struct.pack("!I", code)
+        reply += struct.pack("!I", len(data))
+        reply += data
+        conn.send(reply)
 
     def handshake(self, conn, addr):
-        handshake = 'NBDMAGIC'
-        conn.send(handshake)
+        # Mostly taken from https://github.com/yoe/nbd/blob/master/nbd-server.c
+        conn.send('NBDMAGIC')
         # 0x49484156454F5054
-        handshake = 'IHAVEOPT'
-        # fixed newstyle and nozeros
-        handshake += struct.pack("!H", 3)
-        conn.send(handshake)
+        conn.send('IHAVEOPT')
+        # NBD_FLAG_FIXED_NEWSTYLE | NBD_FLAG_NO_ZEROES
+        conn.send(struct.pack("!H", 3))
 
-    def exportMount(self, conn, addr):
-        [flags] = struct.unpack("!I", conn.recv(4))
-        newstyle = flags & 1
-        nozero = flags & 2
-        # magic
-        conn.recv(8)
-        [option] = struct.unpack("!I", conn.recv(4))
-        if option != 1:
-            #Isn't a NBD_OPT_EXPORT_NAME
+        [cflags] = struct.unpack("!I", conn.recv(4))
+
+        op = 0
+        while op != 1: # NBD_OPT_EXPORT_NAME
+            [magic] = struct.unpack("!Q", conn.recv(8))
+            [op] = struct.unpack("!I", conn.recv(4))
+            if op != 1:
+                # NBD_REP_ERR_UNSUP
+                self.sendreply(conn, addr, 2**31+1, '')
+
+        [namelen] = struct.unpack("!I", conn.recv(4))
+        name = conn.recv(namelen)
+        if name != self.bd:
             conn.close()
             return 1
 
-        [exportlen] = struct.unpack("!I", conn.recv(4))
-        export = conn.recv(exportlen)
-        if export != self.bd:
-            conn.close()
-            return 1
-
-        self.logger.debug('Received request for %s from %s', export, addr)
+        self.logger.debug('Received request for %s from %s', name, addr)
 
         # size of export
         exportinfo = struct.pack('!Q', self.bdsize)
-        flags = 1 # has flags
-        flags |= 1 << (self.mode == 'r') # readonly?
+        flags = int((self.mode == 'r')) # readonly?
         exportinfo += struct.pack('!H', flags)
-        exportinfo += "\x00"*(0 if nozero else 124)
+        exportinfo += "\x00"*(0 if (cflags&2) else 124)
         conn.send(exportinfo)
 
     def handleClient(self, conn, addr):
-        self.handshake(conn, addr)
-        ret = self.exportMount(conn, addr)
+        ret = self.handshake(conn, addr)
         if ret: return # client did something wrong, so we closed them
 
         while True:
-            # atrocious. but works.
+            # atrocious. but works. MSG_WAITALL seems non-functional
             while not conn.recv(4): pass
             [opcode, handle, offset, length] = struct.unpack("!IQQI", conn.recv(24))
             if opcode not in (0, 1, 2):
                 # NBD_REP_ERR_UNSUP
-                response = struct.pack("!I", 0x67446698)
-                response += struct.pack("!I", opcode)
-                response += struct.pack("!I", 2**31 + 1) # NBD_REP_ERR_UNSUP
-                response += struct.pack("!I", 0) # length
-                conn.send(response)
+                self.sendreply(conn, addr, 2**31+1, '')
+                continue
             if opcode == 0: # READ
                 self.openbd.seek(offset)
                 data = self.openbd.read(length)
                 response = struct.pack("!I", 0x67446698)
                 response += struct.pack("!I", 0) # error
                 response += struct.pack("!Q", handle)
-                response += data
                 conn.send(response)
+                conn.send(data)
                 self.logger.debug('%s read %d bytes from %s', addr, length, hex(offset))
             elif opcode == 1: # WRITE
                 data = conn.recv(length)
-                self.logger.debug('%s wrote %d bytes from %s', addr, length, hex(offset))
+                self.logger.debug('%s wrote %d bytes to %s', addr, length, hex(offset))
                 pass
             elif opcode == 2: # DISCONNECT
                 conn.close()
--- pypxe/nbd/test.py
+++ pypxe/nbd/test.py
@@ -3,7 +3,7 @@ import nbd
 args = {
         'blockdevice':'blank.img',
         'mode_debug':True,
-        'mode':'w'
+        'mode':'r'
         }
 
 nbd.NBD(**args).listen()
