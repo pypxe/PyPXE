b6505c711737e778578f0243f65b0ef2c371089c
PsychoMario <pink.banana.fish@gmail.com>
2015-04-18 15:05:48 +0100

    added bootinfo to test.py

--- pypxe/nbd/test.py
+++ pypxe/nbd/test.py
@@ -1,5 +1,8 @@
 import nbd
-
+# kernel cmdline options:
+# https://wiki.archlinux.org/index.php/Syslinux#Pxelinux
+# initrd build config:
+# https://wiki.archlinux.org/index.php/Diskless_system#Bootstrapping_installation
 args = {
         'blockdevice':'blank.img',
         'mode_debug':True,
