7e3f064ff14a0ab5c860d7cf02c94ecf02f9c3d4
PsychoMario <pink.banana.fish@gmail.com>
2015-04-21 19:06:10 +0100

    TFTP class based rewrite

--- pypxe/tftp.py
+++ pypxe/tftp.py
@@ -10,7 +10,177 @@ import os
 import select
 import time
 import logging
-from collections import defaultdict
+import math
+#from collections import defaultdict
+
+class Psocket(socket.socket):
+    '''Subclassed socket.socket to enable a link back to the client object'''
+    parent = None
+
+class Client:
+    '''Client instance for TFTPD.'''
+    def __init__(self, mainsock, parent):
+        # main socket recieve `message`
+        # self.ongoing.append(Client(message, retries, timeout))
+        # select from main socket + [x.sock for x in self.ongoing]
+        self.default_retries = parent.default_retries
+        self.timeout = parent.timeout
+        self.ip = parent.ip
+        self.message, self.address = mainsock.recvfrom(1024)
+        self.logger = parent.logger.getChild('Client.{}'.format(self.address))
+        self.logger.debug('TFTP recieving request')
+        self.retries = self.default_retries
+        self.block = 1
+        self.blksize = 512
+        self.sent_time = float("inf")
+        self.dead = False
+        self.fh = None
+        self.filename = ''
+
+        # message from the main socket
+        self.handle()
+
+    def ready(self):
+        '''Called when there is something to be read on our socket'''
+        self.message = self.sock.recv(1024)
+        self.handle()
+
+    def sendBlock(self):
+        '''Send the next block of data, setting the timeout and retry variables
+        accordingly.'''
+        data = self.fh.read(self.blksize)
+        # opcode 3 == DATA, wraparound block number
+        response = struct.pack("!HH", 3, self.block % 65536)
+        response += data
+        self.sock.sendto(response, self.address)
+        self.logger.debug("Sending block %d", self.block)
+        self.retries -= 1
+        self.sent_time = time.time()
+
+    def noACK(self):
+        '''Have we timed out waiting for an ACK from the client?'''
+        if self.sent_time + self.timeout < time.time():
+            return True
+        return False
+
+    def noRetries(self):
+        '''Has the client ran out of retry attempts'''
+        if not self.retries:
+            return True
+        return False
+
+    def validMode(self):
+        '''Is the file read mode octet? If not, send an error'''
+        mode = self.message.split(chr(0))[1]
+        if mode == "octet": return True
+        self.sendError(5, 'Mode {} not supported'.format(mode))
+        return False
+
+    def checkFile(self):
+        '''Does the file exist and is it a file. If not, send an error'''
+        filename = self.message.split(chr(0))[0]
+        if os.path.lexists(filename) and os.path.isfile(filename):
+            self.filename = filename
+            return True
+        self.sendError(1, 'File Not Found')
+        return False
+
+    def parseOptions(self):
+        '''Extract the options sent from a client, if any, calculate the last
+        block based on the filesize and blocksize'''
+        options = self.message.split(chr(0))[2: -1]
+        options = dict(zip(options[0::2], map(int, options[1::2])))
+        self.blksize = options.get('blksize', self.blksize)
+        self.lastblock = math.ceil(self.filesize / float(self.blksize))
+        self.tsize = True if 'tsize' in options else False
+        if self.filesize > (2**16)*self.blksize:
+            self.logger.warning('TFTP request too big, attempting transfer anyway.')
+            self.logger.debug('  Details: Filesize %s is too big for blksize %s.\n', self.filesize, self.blksize)
+
+        if len(options):
+            # we need to know later if we actually had any options
+            self.block = 0
+            return True
+        else:
+            return False
+
+    def replyOptions(self):
+        '''If we got sent options, we need to ack them'''
+        # only called if options, so send them all
+        response = struct.pack("!H", 6)
+
+        response += 'blksize' + chr(0)
+        response += str(self.blksize) + chr(0)
+        response += 'tsize' + chr(0)
+        response += str(self.filesize) + chr(0)
+
+        self.sock.sendto(response, self.address)
+
+    def newRequest(self):
+        '''Called when we get a read request from the parent socket. Open our
+        own socket and check the read request. If we don't have any options,
+        send the first block'''
+        self.sock = Psocket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.ip, 0))
+        # used by select() to find ready clients
+        self.sock.parent = self
+
+        if not self.validMode() or not self.checkFile():
+            # some clients just ACK the error (wrong code?)
+            # so forcefully shutdown
+            self.complete()
+            return
+
+        self.fh = open(self.filename, 'rb')
+        self.filesize = os.path.getsize(self.filename)
+
+        if not self.parseOptions():
+            # no options recieved so start transfer
+            if self.block == 1:
+                self.sendBlock()
+            return
+
+        # we got some options, so ack those first
+        self.replyOptions()
+
+    def sendError(self, code = 1, message = "File Not Found"):
+        '''Send an error code and string to a client'''
+        response =  struct.pack('!H', 5) # error opcode
+        response += struct.pack('!H', code) # error code
+        response += message
+        response += chr(0)
+        self.sock.sendto(response, self.address)
+        self.logger.debug("TFTP Sending '%d: %s'", code, message)
+
+    def complete(self):
+        '''When we've finished sending a file, we need to close it, the
+        socket, and mark ourselves as dead to be cleaned up'''
+        self.fh.close()
+        self.sock.close()
+        self.dead = True
+
+    def handle(self):
+        '''Take the message from the parent socket and act accordingly'''
+        #if addr not in ongoing, call this, else ready()
+        [opcode] = struct.unpack("!H", self.message[:2])
+        if opcode == 1:
+            self.message = self.message[2:]
+            self.newRequest()
+        elif opcode == 4:
+            [block] = struct.unpack("!H", self.message[2:4])
+            if block < self.block:
+                self.logger.warning('Ignoring duplicated ACK received for block %d', self.block)
+            elif block > self.block:
+                self.logger.warning('Ignoring out of sequence ACK received for block %d', self.block)
+            elif block == self.lastblock:
+                self.logger.debug("Completed sending %s", self.filename)
+                self.complete()
+            else:
+                self.block = block + 1
+                self.retries = self.default_retries
+                self.sendBlock()
+
 
 class TFTPD:
     '''
@@ -45,164 +215,28 @@ class TFTPD:
         self.logger.debug('TFTP Server Port: {}'.format(self.port))
         self.logger.debug('  TFTP Network Boot Directory: {}'.format(self.netbootDirectory))
 
-        #key is (address, port) pair
-        self.ongoing = defaultdict(lambda: {'filename': '', 'handle': None, 'block': 1, 'blksize': 512, 'sock': None, 'sent_time': float("inf"), 'retries': self.default_retries})
+        self.ongoing = []
 
         # Start in network boot file directory and then chroot, 
         # this simplifies target later as well as offers a slight security increase
         os.chdir (self.netbootDirectory)
         os.chroot ('.')
 
-    def filename(self, message):
-        '''
-            The first null-delimited field
-            is the filename. This method returns the filename
-            from the message.
-        '''
-        return message.split(chr(0))[0]
-
-    def tftpError(self, address, code=1, message="File Not Found"):
-        '''
-            short int 5 -> Error Opcode
-            This method sends the error message to the client
-
-            Error codes from RFC1350 page 10:
-            Value     Meaning
-            0         Not defined, see error message (if any).
-            1         File not found.
-            2         Access violation.
-            3         Disk full or allocation exceeded.
-            4         Illegal TFTP operation.
-            5         Unknown transfer ID.
-            6         File already exists.
-            7         No such user.
-        '''
-        response =  struct.pack('!H', 5) # error opcode
-        response += struct.pack('!H', code) # error code
-        response += message
-        response += chr(0)
-        self.logger.debug("TFTP Sending '{code}: {message}'".format(code = code, message = message))
-        self.sock.sendto(response, address)
-
-    def mode(self, message):
-        '''
-            The second null-delimited field
-            is the transfer mode. This method returns the mode
-            from the message.
-        '''
-        return message.split(chr(0))[1]
-
-
-    def sendBlock(self, address):
-        '''
-            short int 3 -> Data Block
-        '''
-        descriptor = self.ongoing[address]
-        response = struct.pack('!H', 3) #opcode 3 is DATA, also sent block number
-        response += struct.pack('!H', descriptor['block'] % 2 ** 16)
-        data = descriptor['handle'].read(descriptor['blksize'])
-        response += data
-        self.logger.debug('TFTP Sending block {block} to client {ip}:{port}'.format(block = repr(descriptor['block']), ip = address[0], port = address[1]))
-        descriptor['sock'].sendto(response, address)
-        self.ongoing[address]['retries'] -= 1
-        self.ongoing[address]['sent_time'] = time.time()
-        if len(data) != descriptor['blksize']:
-            descriptor['handle'].close()
-            self.logger.debug('TFTP File Sent - tftp://{filename} -> {address[0]}:{address[1]}'.format(filename = descriptor['filename'], address = address))
-            descriptor['sock'].close()
-            self.ongoing.pop(address)
-
-    def read(self, address, message):
-        '''
-            On RRQ OPCODE:
-                file exists -> reply with file
-                file does not exist -> reply with error
-        '''
-        mode = self.mode(message)
-        if mode != 'octet':
-            self.logger.error("Mode '{mode}' not supported".format(mode = mode))
-            self.tftpError(address, 5, 'Mode {mode} not supported'.format(mode = mode))
-            return
-        filename = self.filename(message)
-        if not os.path.lexists(filename):
-            self.logger.debug("File '{filename}' not found, sending error message to the client".format(filename = filename) )
-            self.tftpError(address, 1, 'File Not Found')
-            return
-
-        self.ongoing[address]['filename'] = filename
-        self.ongoing[address]['handle'] = open(filename, 'r')
-        options = message.split(chr(0))[2: -1]
-        options = dict(zip(options[0::2], options[1::2]))
-        response = ''
-        if 'blksize' in options:
-            response += 'blksize' + chr(0)
-            response += options['blksize']
-            response += chr(0)
-            self.ongoing[address]['blksize'] = int(options['blksize'])
-        filesize = os.path.getsize(self.ongoing[address]['filename'])
-        if filesize > (2**16 * self.ongoing[address]['blksize']):
-            self.logger.warning('TFTP request too big, attempting transfer anyway.')
-            self.logger.warning('  Details: Filesize {filesize} is too big for blksize {blksize}.\n'.format(filesize = filesize, blksize = self.ongoing[address]['blksize']))
-        if 'tsize' in options:
-            response += 'tsize' + chr(0)
-            response += str(filesize)
-            response += chr(0)
-
-        # Create the data socket only if needed
-        if self.ongoing[address]['sock'] == None:
-            socknew = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            socknew.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            socknew.bind((self.ip, 0))
-            self.ongoing[address]['sock'] = socknew
-
-        if response:
-            response = struct.pack('!H', 6) + response
-            socknew.sendto(response, address)
-        # if no options in request and no block sent yet start sending data
-        elif self.ongoing[address]['block'] == 1:
-            self.sendBlock(address)
-        else:
-            self.logger.warning('Ignored TFTP request: no options in a middle of a transmission')
 
     def listen(self):
         '''This method listens for incoming requests'''
         while True:
-            rlist, wlist, xlist = select.select([self.sock] + [self.ongoing[i]['sock'] for i in self.ongoing if self.ongoing[i]['sock']], [], [], 0)
+            # Remove complete clients to select doesn't fail
+            map(self.ongoing.remove, [client for client in self.ongoing if client.dead])
+            rlist, _, _ = select.select([self.sock] + [client.sock for client in self.ongoing if not client.dead], [], [], 0)
             for sock in rlist:
-                message, address = sock.recvfrom(1024)
-                opcode = struct.unpack('!H', message[:2])[0]
-                message = message[2:]
-                if opcode == 1: # read the request
-                    self.logger.debug('TFTP receiving request')
-                    self.read(address, message)
-                if opcode == 4: # ack
-                    if self.ongoing.has_key(address):
-                        blockack = struct.unpack("!H", message[:2])[0]
-                        if blockack < self.ongoing[address]['block']:
-                            self.logger.warning('Ignoring duplicated ACK received for block {blockack}'.format(blockack = blockack))
-                            continue
-                        if blockack > self.ongoing[address]['block']:
-                            self.logger.warning('Ignoring out of sequence ACK received for block {blockack}'.format(blockack = blockack))
-                            continue
-                        self.ongoing[address]['block'] = blockack + 1
-                        self.ongoing[address]['retries'] = self.default_retries
-                        self.sendBlock(address)
-
-            # Timeouts and Retries. Done after the above so timeout actually has a value
-            # Resent those that have timed out
-            dead_conn = []
-            for i in self.ongoing:
-                if self.ongoing[i]['sent_time'] + self.timeout < time.time() and self.ongoing[i]['retries']:
-                    self.logger.debug('No ACK received for block: {block}, retrying'.format(block = self.ongoing[address]['block']))
-                    self.ongoing[i]['handle'].seek(-self.ongoing[i]['blksize'], 1)
-                    self.sendBlock(i)
-                if not self.ongoing[i]['retries']:
-                    self.logger.debug('Max retries reached, aborting connection with {client}:{port}'.format(client = i[0], port = i[1]))
-                    self.tftpError(i, 0, 'Timeout reached')
-                    self.ongoing[i]['handle'].close()
-                    self.ongoing[i]['sock'].close()
-                    dead_conn.append(i)
-
-            # Clean up dead connections
-            for i in dead_conn:
-                self.ongoing.remove(i)
+                if sock == self.sock:
+                    # main socket, so new client
+                    self.ongoing.append(Client(sock, self))
+                else:
+                    # client socket, so tell the client object it's ready
+                    sock.parent.ready()
+            # If we haven't recieved an ACK in timeout time, retry
+            [client.sendBlock() for client in self.ongoing if client.noACK()]
+            # If we have run out of retries, kill the client.
+            [client.complete() for client in self.ongoing if client.noRetries()]
